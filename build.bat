c3f07e8f03b31d13b6160ff1046014f681302c95
mwrock <none@none>
2011-10-09 09:36:18 -0700

    updated build.bat to not wipe build directory when pushing nuget

--- build.bat
+++ build.bat
@@ -5,6 +5,7 @@ set Configuration=Release
 if /i "%1"=="DEBUG" set Configuration=Debug
 if /i "%2"=="DEBUG" set Configuration=Debug
 if /i "%1"=="RESET" goto net40
+if /i "%1"=="PUSH-NUGET" goto net40
 
 echo abc | powershell -NonInteractive -NoProfile -ExecutionPolicy unrestricted -Command "%~dp0\packages\psake.4.0.1.0\tools\psake.ps1 .\BuildScript.ps1 -properties @{configuration='%Configuration%'} -framework '3.5' BuildNet35"
 
