8f2e2a7f9da86cfcb390490db46cce54ce03f961
Drew Noakes <git@drewnoakes.com>
2013-11-29 18:10:03 +0000

    Fixed typo.

--- nQuant.Core/WuQuantizerBase.cs
+++ nQuant.Core/WuQuantizerBase.cs
@@ -97,7 +97,7 @@ namespace nQuant
             {
                 var bitDepth = Image.GetPixelFormatSize(sourceImage.PixelFormat);
                 if (bitDepth != 32)
-                    throw new QuantizationException(string.Format("Thie image you are attempting to quantize does not contain a 32 bit ARGB palette. This image has a bit depth of {0} with {1} colors.", bitDepth, sourceImage.Palette.Entries.Length));
+                    throw new QuantizationException(string.Format("The image you are attempting to quantize does not contain a 32 bit ARGB palette. This image has a bit depth of {0} with {1} colors.", bitDepth, sourceImage.Palette.Entries.Length));
                 var byteLength = data.Stride < 0 ? -data.Stride : data.Stride;
                 var byteCount = Math.Max(1, bitDepth >> 3);
                 var offset = 0;
